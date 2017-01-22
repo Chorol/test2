e4f4fe49373cac14862a597ace15f8032374a779
CrazyIvanftw <gbaustin89@gmail.com>
2017-01-22 14:01:46 +0100

    Local Commit Test

---
+++ JavaCodeTest2/src/test2Package/JavaTest2Class.java
@@ -0,0 +1,15 @@
+package test2Package;
+
+public class JavaTest2Class {
+	
+	String s = "This is a test... It works!";
+	
+	public JavaTest2Class(){
+		
+	}
+	
+	public void doesItWork(){
+		System.out.println(s);
+	}
+
+}
